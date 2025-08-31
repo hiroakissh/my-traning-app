f6723046bf81dbc3820957675501ca6b401ec823
hiroakissh <hiroaki.saito1014@gmail.com>
2025-08-31 17:33:34 +0900

    feat: プロジェクト追加

---
+++ my-traning-app/my-traning-appTests/my_traning_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  my_traning_appTests.swift
+//  my-traning-appTests
+//
+//  Created by HiroakiSaito on 2025/08/31.
+//
+
+import Testing
+
+struct my_traning_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ my-traning-app/my-traning-appUITests/my_traning_appUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  my_traning_appUITestsLaunchTests.swift
+//  my-traning-appUITests
+//
+//  Created by HiroakiSaito on 2025/08/31.
+//
+
+import XCTest
+
+final class my_traning_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
