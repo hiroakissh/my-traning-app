f42ed22c2ae534942d40cd0d396ca259e7bbad69
Hiroaki Saito <48234687+hiroakissh@users.noreply.github.com>
2025-12-27 11:23:04 +0900

    Use in-memory SwiftData store during tests

--- my-traning-app/my-traning-app/Application/my_traning_appApp.swift
+++ my-traning-app/my-traning-app/Application/my_traning_appApp.swift
@@ -6,12 +6,32 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct my_traning_appApp: App {
+    private let sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            TrainingLog.self,
+            TrainingCondition.self,
+            TrainingExercise.self,
+            TrainingSet.self
+        ])
+
+        let isRunningTests = ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil
+        let configuration = ModelConfiguration(isStoredInMemoryOnly: isRunningTests)
+
+        do {
+            return try ModelContainer(for: schema, configurations: configuration)
+        } catch {
+            fatalError("Failed to create ModelContainer: \(error)")
+        }
+    }()
+
     var body: some Scene {
         WindowGroup {
             MainView()
         }
+        .modelContainer(sharedModelContainer)
     }
 }
